593edc03b0fdf37df57145957de4aa5d0dc52ea6
FERNANDACHAVEZ04 <fernandachagac@gmail.com>
2025-10-07 17:55:35 -0600

    Cambios

--- avisos/urls.py
+++ avisos/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import AvisosVigentesListView
+from .views import landing_page
 
 urlpatterns = [
     path('', AvisosVigentesListView.as_view(), name='lista_avisos'),
+    path("",landing_page)
 ]
+
+
--- avisos/views.py
+++ avisos/views.py
@@ -1,7 +1,13 @@
 from django.views.generic import ListView
 from django.utils import timezone
 from .models import Aviso
+from django.shortcuts import render
 
+# Función para la página de inicio
+def landing_page(request):
+    return render(request, "landing.html")
+
+# Vista basada en clase para lista de avisos
 class AvisosVigentesListView(ListView):
     model = Aviso
     template_name = "avisos/lista_avisos.html"
